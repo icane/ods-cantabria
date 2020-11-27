645a601acc0db242820d13f6e71059d0f579c9c8
Cecilio Alonso <alonso_cec@cantabria.es>
2020-11-27 10:42:07 +0100

    Update goal-by-target-vertical.html

---
+++ _includes/assets/js/plugins/leaflet.yearSlider.js
@@ -0,0 +1,128 @@
+/*
+ * Leaflet year Slider.
+ *
+ * This is merely a specific configuration of Leaflet of L.TimeDimension.
+ * See here: https://github.com/socib/Leaflet.TimeDimension
+ */
+(function () {
+  "use strict";
+
+  if (typeof L === 'undefined') {
+    return;
+  }
+
+  var defaultOptions = {
+    // YearSlider options.
+    yearChangeCallback: null,
+    years: [],
+    // TimeDimensionControl options.
+    timeSliderDragUpdate: true,
+    speedSlider: false,
+    position: 'bottomleft',
+    // Player options.
+    playerOptions: {
+      transitionTime: 1000,
+      loop: false,
+      startOver: true
+    },
+  };
+
+  L.Control.YearSlider = L.Control.TimeDimension.extend({
+
+    // Hijack the displayed date format.
+    _getDisplayDateFormat: function(date){
+      return date.getFullYear();
+    },
+
+    // Override the _createButton method to prevent the date from being a link.
+    _createButton: function(title, container) {
+      if (title === 'Date') {
+        var span = L.DomUtil.create('span', this.options.styleNS + ' timecontrol-' + title.toLowerCase(), container);
+        span.title = title;
+        return span;
+      }
+      else {
+        return L.Control.TimeDimension.prototype._createButton.call(this, title, container);
+      }
+    },
+
+    // Override the _createSliderTime method to give the slider accessibility features.
+    _createSliderTime: function(className, container) {
+      var knob = L.Control.TimeDimension.prototype._createSliderTime.call(this, className, container),
+          control = this,
+          times = this._timeDimension.getAvailableTimes(),
+          years = times.map(function(time) {
+            var date = new Date(time);
+            return control._getDisplayDateFormat(date);
+          }),
+          minYear = years[0],
+          maxYear = years[years.length - 1],
+          knobElement = knob._element;
+
+      knobElement.setAttribute('tabindex', '0');
+      knobElement.setAttribute('role', 'slider');
+      knobElement.setAttribute('aria-label', 'Year slider');
+      knobElement.setAttribute('aria-valuemin', minYear);
+      knobElement.setAttribute('aria-valuemax', maxYear);
+
+      function updateSliderAttributes() {
+        var yearIndex = 0;
+        if (knob.getValue()) {
+          yearIndex = knob.getValue();
+        }
+        knobElement.setAttribute('aria-valuenow', years[yearIndex]);
+      }
+      updateSliderAttributes();
+
+      // Give the slider left/right keyboard functionality.
+      knobElement.addEventListener('keydown', function(e) {
+        if (e.which === 37 || e.which === 40) {
+          var min = knob.getMinValue();
+          var value = knob.getValue();
+          value = value - 1;
+          if (value >= min) {
+            knob.setValue(value);
+            control._sliderTimeValueChanged(value);
+            updateSliderAttributes();
+          }
+          e.preventDefault();
+        }
+        else if (e.which === 39 || e.which === 38) {
+          var max = knob.getMaxValue();
+          var value = knob.getValue();
+          value = value + 1;
+          if (value <= max) {
+            knob.setValue(value);
+            control._sliderTimeValueChanged(value);
+            updateSliderAttributes();
+          }
+          e.preventDefault();
+        }
+      });
+      return knob;
+    }
+
+  });
+
+  // Helper function to compose the full widget.
+  L.Control.yearSlider = function(options) {
+    // Extend the defaults.
+    options = L.Util.extend(defaultOptions, options);
+    // Hardcode the timeDimension to year intervals.
+    options.timeDimension = new L.TimeDimension({
+      // We pad our years to at least January 2nd, so that timezone issues don't
+      // cause any problems. This converts the array of years into a comma-
+      // delimited string of YYYY-MM-DD dates.
+      times: options.years.join('-01-02,') + '-01-02',
+      currentTime: new Date(options.years[0] + '-01-02').getTime(),
+    });
+    // Create the player.
+    options.player = new L.TimeDimension.Player(options.playerOptions, options.timeDimension);
+    // Listen for time changes.
+    if (typeof options.yearChangeCallback === 'function') {
+      options.timeDimension.on('timeload', options.yearChangeCallback);
+    };
+    // Return the control.
+    return new L.Control.YearSlider(options);
+  };
+}());
