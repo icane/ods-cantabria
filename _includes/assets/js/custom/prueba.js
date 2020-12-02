71f93f9172dd3c4a9984898f1762a526a72f6661
Cecilio Alonso <alonso_cec@cantabria.es>
2020-12-02 15:14:12 +0100

    Test custom js

--- _includes/assets/js/custom/prueba.js
+++ _includes/assets/js/custom/prueba.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function(event) {
     console.log("'Todos los recursos terminaron de cargar!");
     prueba = document.getElementById('indicator-main');
-    console.log(prueba);
+    if (prueba != null){
+        window.location.href = '#tableview';
+    }
 });
