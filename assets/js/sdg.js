29e8e3490ba9cb961e7e237d0b8892f6ded89c03
Cecilio Alonso <alonso_cec@cantabria.es>
2020-12-02 14:04:50 +0100

    Test custom js

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -18,4 +18,4 @@
 {%- include assets/js/plugins/leaflet.selectionLegend.js -%}
 {%- include assets/js/plugins/leaflet.yearSlider.js -%}
 {%- include assets/js/googleAnalytics.js -%}
-{%- include prueba.js -%}
+{%- prueba.js -%}
