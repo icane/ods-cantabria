772f2e635ee446d2aa003a760523cdac29e76a07
Cecilio Alonso <alonso_cec@cantabria.es>
2020-12-03 07:45:36 +0100

    Modify tab table data by default

--- _includes/assets/js/custom/prueba.js
+++ _includes/assets/js/custom/prueba.js
@@ -1,9 +1,9 @@
-window.addEventListener("load", function(event) {
-    console.log("'Todos los recursos terminaron de cargar!");
-    prueba = document.getElementById('indicator-main');
-    if (prueba != null){
-        //window.location.href = '#tableview';
-        $('[href="#tableview"]').tab('show');
-        console.log('_');
-    }
-});
+// window.addEventListener("load", function(event) {
+//     console.log("'Todos los recursos terminaron de cargar!");
+//     prueba = document.getElementById('indicator-main');
+//     if (prueba != null){
+//         //window.location.href = '#tableview';
+//         $('[href="#tableview"]').tab('show');
+//         console.log('testing');
+//     }
+// });
