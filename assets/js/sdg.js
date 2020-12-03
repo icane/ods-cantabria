f7bee3f5e0f9729bc9d61d257e218713593ddf80
arc17854 <71650807+arc17854@users.noreply.github.com>
2020-12-03 07:17:06 +0000

    Deploying to gh-pages from  @ e8c0798c69996a9574640460731c3659f2235cd8 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3091,12 +3091,3 @@ function sendPageviewToGoogleAnalytics(){
 }
 
 
-// window.addEventListener("load", function(event) {
-//     console.log("'Todos los recursos terminaron de cargar!");
-//     prueba = document.getElementById('indicator-main');
-//     if (prueba != null){
-//         //window.location.href = '#tableview';
-//         $('[href="#tableview"]').tab('show');
-//         console.log('testing');
-//     }
-// });
