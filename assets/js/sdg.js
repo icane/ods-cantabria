9be41264b884184ccbd9654f625f1d7844dfb01f
arc17854 <71650807+arc17854@users.noreply.github.com>
2020-12-03 06:53:23 +0000

    Deploying to gh-pages from  @ 2f2aaa85a6ef085c10064aba9f1586174ab91a95 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3091,12 +3091,12 @@ function sendPageviewToGoogleAnalytics(){
 }
 
 
-window.addEventListener("load", function(event) {
-    console.log("'Todos los recursos terminaron de cargar!");
-    prueba = document.getElementById('indicator-main');
-    if (prueba != null){
-        //window.location.href = '#tableview';
-        $('[href="#tableview"]').tab('show');
-        console.log('_');
-    }
-});
+// window.addEventListener("load", function(event) {
+//     console.log("'Todos los recursos terminaron de cargar!");
+//     prueba = document.getElementById('indicator-main');
+//     if (prueba != null){
+//         //window.location.href = '#tableview';
+//         $('[href="#tableview"]').tab('show');
+//         console.log('testing');
+//     }
+// });
